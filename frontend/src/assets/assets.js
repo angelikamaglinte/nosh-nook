9d8750e8837fe1dd14b1c5fdd48c62332e4cb612
Angelika Maglinte <53634332+angelikamaglinte@users.noreply.github.com>
2024-07-11 20:33:49 -0600

    Updated assets.js

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,9 +1,16 @@
-import basket_icon from './basket_icon.png'
-import logo from './logo.png'
+// logo
 import noshnnooklogo_black from './noshnnook_logo_black.png'
 import noshnnooklogo_white from './noshnnook_logo_white.png'
-import header_img from './header_img.png'
+
+// search and basket icons
+import basket_icon from './basket_icon.png'
 import search_icon from './search_icon.png'
+import profile_icon from './profile_icon.png'
+
+// header
+import header_img from './header_image1.jpeg'
+
+// menu 
 import menu_1 from './menu_1.png'
 import menu_2 from './menu_2.png'
 import menu_3 from './menu_3.png'
@@ -13,103 +20,72 @@ import menu_6 from './menu_6.png'
 import menu_7 from './menu_7.png'
 import menu_8 from './menu_8.png'
 
-import food_1 from './food_1.png'
-import food_2 from './food_2.png'
-import food_3 from './food_3.png'
-import food_4 from './food_4.png'
-import food_5 from './food_5.png'
-import food_6 from './food_6.png'
-import food_7 from './food_7.png'
-import food_8 from './food_8.png'
-import food_9 from './food_9.png'
-import food_10 from './food_10.png'
-import food_11 from './food_11.png'
-import food_12 from './food_12.png'
-import food_13 from './food_13.png'
-import food_14 from './food_14.png'
-import food_15 from './food_15.png'
-import food_16 from './food_16.png'
-import food_17 from './food_17.png'
-import food_18 from './food_18.png'
-import food_19 from './food_19.png'
-import food_20 from './food_20.png'
-import food_21 from './food_21.png'
-import food_22 from './food_22.png'
-import food_23 from './food_23.png'
-import food_24 from './food_24.png'
-import food_25 from './food_25.png'
-import food_26 from './food_26.png'
-import food_27 from './food_27.png'
-import food_28 from './food_28.png'
-import food_29 from './food_29.png'
-import food_30 from './food_30.png'
-import food_31 from './food_31.png'
-import food_32 from './food_32.png'
-
+// icons
 import add_icon_white from './add_icon_white.png'
 import add_icon_green from './add_icon_green.png'
 import remove_icon_red from './remove_icon_red.png'
-import app_store from './app_store.png'
-import play_store from './play_store.png'
 import cross_icon from './cross_icon.png'
 import selector_icon from './selector_icon.png'
 import rating_starts from './rating_starts.png'
-import profile_icon from './profile_icon.png'
 import bag_icon from './bag_icon.png'
 import logout_icon from './logout_icon.png'
 import parcel_icon from './parcel_icon.png'
 
+// app store and play store
+import app_store from './app_store.png'
+import play_store from './play_store.png'
+
+// social media icons
 import facebook_icon from './facebook_icon.png'
 import instagram_icon from './instagram_icon.png'
 import youtube_icon from './youtube_icon.png'
 
 export const assets = {
-    logo,
     noshnnooklogo_black,
     noshnnooklogo_white,
     basket_icon,
-    header_img,
     search_icon,
-    rating_starts,
-    add_icon_green,
+    profile_icon,
+    header_img,
     add_icon_white,
+    add_icon_green,
     remove_icon_red,
+    cross_icon,
+    selector_icon,
+    rating_starts,
+    bag_icon,
+    logout_icon,
+    parcel_icon,
     app_store,
     play_store,
     facebook_icon,
     instagram_icon,
-    youtube_icon,
-    cross_icon,
-    selector_icon,
-    profile_icon,
-    logout_icon,
-    bag_icon,
-    parcel_icon
+    youtube_icon
 }
 
 export const menu_list = [
     {
-        menu_name: "Salad",
+        menu_name: "Coffee",
         menu_image: menu_1
     },
     {
-        menu_name: "Rolls",
+        menu_name: "Sandwich",
         menu_image: menu_2
     },
     {
-        menu_name: "Deserts",
+        menu_name: "Pastries",
         menu_image: menu_3
     },
     {
-        menu_name: "Sandwich",
+        menu_name: "Cakes",
         menu_image: menu_4
     },
     {
-        menu_name: "Cake",
+        menu_name: "Donuts",
         menu_image: menu_5
     },
     {
-        menu_name: "Pure Veg",
+        menu_name: "Parfait",
         menu_image: menu_6
     },
     {
@@ -117,7 +93,7 @@ export const menu_list = [
         menu_image: menu_7
     },
     {
-        menu_name: "Noodles",
+        menu_name: "Salad",
         menu_image: menu_8
     }]
 
