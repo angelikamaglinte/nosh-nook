a2628397bd006e07bfac3d9803797e1886b4ae62
Angelika Maglinte <53634332+angelikamaglinte@users.noreply.github.com>
2024-07-11 21:41:57 -0600

    Updated Header.jsx

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
       <div className="header-contents">
         <h2>Order your favorite food here</h2>
         <p>Choose from a rich menu showcasing delectable dishes prepared with the best ingredients and culinary expertise. At Nosh & Nook, our mission is to fulfill your cravings and enhance your dining journey, one savory meal at a time.</p>
-        <button>View Menu</button>
+        <button><a href="#explore-menu">View Menu</a></button>
       </div>
     </div>
   );
