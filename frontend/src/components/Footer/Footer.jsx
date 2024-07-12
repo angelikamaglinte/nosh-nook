7d8e2fd8b52d8d3c27d53fbf64a73700548dea6c
Angelika Maglinte <53634332+angelikamaglinte@users.noreply.github.com>
2024-07-11 18:07:47 -0600

    Updated Footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -7,13 +7,6 @@ const Footer = () => {
         <div className='footer' id='footer'>
             <div className="footer-content">
                 <div className="footer-content-left">
-                    {/* <img src={assets.noshnnooklogo_white} alt="" /> */}
-                    {/* <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p> */}
-                    {/* <div className="footer-social-icons">
-                        <img src={assets.facebook_icon} alt="" />
-                        <img src={assets.twitter_icon} alt="" />
-                        <img src={assets.linkedin_icon} alt="" />
-                    </div> */}
                     <p>Support</p>
                     <ul>
                         <li>Help Centre</li>
@@ -32,7 +25,7 @@ const Footer = () => {
                     </ul>
                 </div>
                 <div className="footer-content-center">
-                    <h2>Get In Touch</h2>
+                    <p>Get In Touch</p>
                     <ul>
                         <li>hello@creativedesignstudio.co.site</li>
                         <li>+1-987-654-3210</li>
